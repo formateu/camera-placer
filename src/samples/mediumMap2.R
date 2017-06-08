0537927fef32af0e221e9c70924b4d98bde4e536
Mateusz Forc <formateu@gmail.com>
2017-06-08 11:44:23 +0200

    Fix map name and initial point

--- src/samples/mediumMap2.R
+++ src/samples/mediumMap2.R
@@ -2,6 +2,6 @@ source("utils.R")
 
 #mediumMap2
 pointsX <- c(1,1,3,3,8,8,10,10,13,13)
-pointsY <- c(4,9,9,7,7,3,3, 7, 7, 1)
+pointsY <- c(1,9,9,7,7,3,3, 7, 7, 1)
 
-runTests("mediumMap", pointsX, pointsY)
+runTests("mediumMap2", pointsX, pointsY)
